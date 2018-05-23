803a30a09a5f7a6f94578c30262242dd7475b289
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2018-05-23 18:32:29 +0530

    Installation done

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 # Installation of suitable packages
 
+pkgInstall()
+{
+  #First install yaourt
+  sudo pacman -S yaourt
+  #Change to root for the change
+  sudo su -
+  echo "[archlinuxfr] 
+    Siglevel = Never 
+    Server = http://repo.archlinux.fr/\$arch " >> /etc/pacman.conf
+  echo " Installing Packages from official Repo... "
+  pacman -S - < paclist
+  clear
+  echo " Installing Packages from AUR..."
+  cat aurlist | while read line; do yaourt -S ${line}; done
+  echo " Installation done..."
+  clear
+}
 
+pkgInstall
 
-#First install yaourt
-
-sudo pacman -S yaourt
-
-#Change to root for the change
-
-sudo su -
-
-echo "[archlinuxfr] 
-Siglevel = Never 
-Server = http://repo.archlinux.fr/\$arch " >> /etc/pacman.conf
-
+bash ./bin/km
