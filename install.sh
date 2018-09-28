9465886049bedb76be0d7f14f3b3caee3f5229f8
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2018-09-28 16:42:05 +0000

    new scripts added

--- install.sh
+++ install.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 # Installation of suitable packages
 
-pkgInstall()
-{
-  #First install yaourt
+ echo "First install yaourt..."
   sudo pacman -S yaourt
-  #Change to root for the change
+ echo "Chrooting..."
   sudo su -
   echo "[archlinuxfr] 
     Siglevel = Never 
     Server = http://repo.archlinux.fr/\$arch " >> /etc/pacman.conf
-    echo "Updating the System"
+    echo "Updating the System first to the recent packages..."
     pacman -Syu
-  echo " Installing Packages from official Repo... "
-  pacman -S - < paclist
-  clear
+  echo " Installing Necessary Packages from official Repo... "
+   pacman -S - < paclist
+
   echo " Installing Packages from AUR..."
   cat aurlist | while read line; do yaourt -S ${line}; done
   echo " Installation done..."
   clear
-}
-
-pkgInstall
-
 bash ./bin/km
